b1197804ce8ec322d68ef12c4f0bc5fbbc4c48da
Denis Dyadyun <sysadm85@gmail.com>
2018-11-24 14:19:29 +0700

    wip

--- index.js
+++ index.js
@@ -7,4 +7,11 @@ RNCloudNotification.on = (eventName, callback) => {
     DeviceEventEmitter.addListener(eventName, callback);
 };
 
+export const EVENT_CHANGE_ROUTE = 'FCMChangeRoute';
+export const EVENT_FCM_NOTIFICATION = '';
+
+export const PRIORITY_URGENT = 'urgent';
+export const PRIORITY_HIGH = 'high';
+export const PRIORITY_MEDIUM = 'default';
+
 export default RNCloudNotification;
